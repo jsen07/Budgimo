ba5092de651f5cf871822470f112fdd2b9e1c272
IrisNyniaa <irisnyniaa@gmail.com>
2025-04-06 22:39:34 +0100

    Add Service + Contact Us Section

---
+++ client/src/components/LandingPageComponent/FAQ.js
@@ -0,0 +1,32 @@
+const FAQ_section=[
+    {
+      question: "What is Budgimo?",
+      answer: "Budgimo is a personal budgeting app that helps you track spending, set savings goals, and stay on top of your finances with ease."
+    },
+    {
+      question: "Is Budgimo free to use?",
+      answer: "Yes! Budgimo offers a free version with powerful tools. We also have a premium version with advanced analytics and personalized insights."
+    },
+    {
+      question: "How do I track my expenses?",
+      answer: "You need to manually add your expenses on the sheet"
+    },
+    {
+      question: "Is my financial data safe?",
+      answer: "Absolutely. Your privacy and security are our top priorities."
+    },
+    {
+      question: "Can I set financial goals in the app?",
+      answer: "Definitely. Budgimo makes it easy to create goals like saving for a vacation, paying off debt, or building an emergency fund."
+    },
+    {
+      question: "Is Budgimo available on Android and iOS?",
+      answer: "Yes, Budgimo is available on both Android and iOS so you can manage your money anytime, anywhere."
+    },
+    {
+      question: "How do I get started?",
+      answer: "Just download the app, create an account, and follow the setup steps. You'll be budgeting like a pro in no time."
+    }
+  ]
+
+  export default FAQ_section
---
+++ client/src/components/LandingPageComponent/Services.js
@@ -0,0 +1,36 @@
+import CreateMonthlyBudgets from '../../assets/images/create_monthly_budgets.jpeg'
+import IncomeTracking from '../../assets/images/income_tracking.jpeg'
+import MonthlySummaryReports from '../../assets/images/monthly_summary_reports.png'
+import SetSavingsGoals from '../../assets/images/set_savings_goals.jpeg'
+import TrackRecurringExpenses from '../../assets/images/track_recurring_expenses.png'
+import TrackSpendingByCategory from '../../assets/images/track_spending_by_category.jpeg'
+
+const Services_LP = [
+    {
+        title: 'Create Monthly Budgets',
+        icon: CreateMonthlyBudgets
+      },
+      {
+        title: 'Track Spending by Category',
+        icon: TrackSpendingByCategory
+      },
+      {
+        title: 'Set Savings Goals',
+        icon: SetSavingsGoals
+      },
+      {
+        title: 'Income Tracking',
+        icon: IncomeTracking
+      },
+      {
+        title: 'Monthly Summary Reports',
+        icon: MonthlySummaryReports
+      },
+      {
+        title: 'Track Recurring Expenses',
+        icon: TrackRecurringExpenses
+      }
+]
+
+
+export default Services_LP
